10b4f92533fa74db0b1b5d11678e98d29af796c8
Jaqueline Romero <csi23-jromres@colegioaltair.net>
2024-02-10 16:51:36 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace CajeroMenu.Controladores
+{
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Introduzca fecha en formato dd/mm/yyyy");    
+            string fecha= Console.ReadLine();
+            DateTime fechaInicial = Convert.ToDateTime(fecha);
+            Console.WriteLine("Introduzca un número de días");
+            int numDias= Convert.ToInt32(Console.ReadLine());
+
+            DateTime difFechas = fechaInicial.AddDays(-numDias);
+            DateTime sumFechas = fechaInicial.AddDays(numDias);
+            Console.WriteLine("El resultado de sumar " + numDias + " dias a la fecha " + fechaInicial + " es: " + sumFechas);
+            Console.WriteLine("El resultado de restar " + numDias + " dias a la fecha " + fechaInicial + " es: " + difFechas);
+
+        }
+    }
+}
